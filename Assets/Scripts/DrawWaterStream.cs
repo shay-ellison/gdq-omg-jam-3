1820aec1ef86064590a352a6b6e650969c617897
Shay <shay.m.ellison@gmail.com>
2018-02-11 17:35:30 -0600

    Water actually dissapears.

--- Assets/Scripts/DrawWaterStream.cs
+++ Assets/Scripts/DrawWaterStream.cs
@@ -4,15 +4,23 @@ using UnityEngine;
 
 public class DrawWaterStream : MonoBehaviour {
     public LineRenderer lineRenderer;
+    private float timeToLive = 0.3f;
+    private bool drawing = false;
 
     public void StartPosition(Vector3 position) {
         Vector3 renderVector = new Vector3(position.x, position.y, 1f);
         lineRenderer.SetPosition(0, renderVector);
+        drawing = true;
+        timeToLive = 0.5f;
+        lineRenderer.enabled = true;
     }
 
     public void EndPosition(Vector3 position) {
         Vector3 renderVector = new Vector3(position.x, position.y, 1f);
         lineRenderer.SetPosition(1, renderVector);
+        drawing = true;
+        timeToLive = 0.5f;
+        lineRenderer.enabled = true;
     }
 
     // Use this for initialization
@@ -31,7 +39,15 @@ public class DrawWaterStream : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		
+		if (drawing) {
+            timeToLive -= Time.deltaTime;
+            if (timeToLive <= 0) {
+                drawing = false;
+                lineRenderer.enabled = false;
+                //lineRenderer.SetPosition(0, new Vector3(0, 0, 0));
+                //lineRenderer.SetPosition(1, new Vector3(0, 0, 0));
+            }
+        }
 	}
 
     void FixedUpdate() {
