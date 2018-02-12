502be31af5b0320626f6d58de3e8441f0cbea9d9
Shay <shay.m.ellison@gmail.com>
2018-02-11 22:39:59 -0600

    Hearts + die.

---
+++ Assets/Scripts/PlayerStats.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerStats : MonoBehaviour {
+
+    public GameObject[] fullHearts;
+    public GameObject[] emptyHearts;
+    
+    public int health = 3;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        GameObject collisionObject = collision.gameObject;
+        if (collisionObject.tag == "Extinguish") {
+            health -= 1;
+            if (health <= 0) {
+                Debug.Log("YOU DEAD");
+                Destroy(gameObject);
+            } else {
+                fullHearts[health].GetComponent<Renderer>().enabled = false;
+            }
+        } else if (collisionObject.tag == "Health") {
+            fullHearts[health].GetComponent<Renderer>().enabled = true;
+            health++;
+            Destroy(collisionObject);
+        }
+    }
+}
